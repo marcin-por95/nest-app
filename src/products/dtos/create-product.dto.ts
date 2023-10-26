f157685060c99d943595019d0dc4abdfd14714e0
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-10-26 12:48:46 +0200

    validation to dto

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,5 +1,24 @@
-export type CreateProductDTO = {
+import { Transform } from 'class-transformer';
+import {
+  IsInt,
+  IsNotEmpty,
+  Length,
+  IsString,
+  Min,
+} from 'class-validator';
+export class CreateProductDTO {
+  @IsNotEmpty()
+  @IsString()
+  @Length(10, 30)
   name: string;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(0)
   price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  @Transform(({ value }) => (Array.isArray(value) ? value.join(', ') : ''))
   description: string;
 };
